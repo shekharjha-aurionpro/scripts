a02e5dd723552599c32f7c8a329c7e0c77771f46
Shekhar Jha <shekhar-jha@users.noreply.github.com>
2017-04-01 09:48:32 -0500

    Support for Oracle DB Monitoring
    Refactored how the arguments are being handled
    Added support for monitoring Oracle Database using simple SQL Query.

--- WebPing.java
+++ WebPing.java
@@ -1,20 +1,36 @@
+import java.io.Console;
 import java.net.Socket;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class WebPing implements Runnable {
 	private List<Long> executionValues = new ArrayList<Long>();
 	private List<Long> connectionValues = new ArrayList<Long>();
+	private List<Long> sqlExecutionValues = new ArrayList<Long>();
 
 	public static void main(String[] args) throws Exception {
 		WebPing webPing = new WebPing();
 		Runtime.getRuntime().addShutdownHook(new Thread(webPing));
+		String serverName = args.length < 1 ? "www.google.com" : args[0];
+		int port = args.length < 2 ? 80 : Integer.parseInt(args[1]);
+		String database = args.length < 3 ? null : args[2];
+		String userName = args.length < 4 ? null : args[3];
+		char[] password = null;
+		if (userName != null) {
+			DriverManager.registerDriver((Driver) Class.forName("oracle.jdbc.driver.OracleDriver").newInstance());
+			password = System.console().readPassword("Database Password :");
+		}
 		while (true) {
-			webPing.executeTest(args.length < 1 ? "www.google.com" : args[0],
-					args.length < 2 ? 80 : Integer.parseInt(args[1]));
-			Thread.sleep(args.length < 3 ? 100 : Integer.parseInt(args[2]));
+			webPing.executeTest(serverName, port);
+			if (userName != null && password != null) {
+				webPing.executeSQLTest("jdbc:oracle:thin:@" + serverName + ":" + port + "/" + database, userName,
+						password);
+			}
+			Thread.sleep(100);
 		}
 	}
 
@@ -38,15 +54,65 @@ public class WebPing implements Runnable {
 		executionValues.add(executionTime);
 	}
 
+	public void executeSQLTest(String jdbcURL, String userName, char[] password) {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet resultSet = null;
+		try {
+			long sqlExecutionStart = System.currentTimeMillis();
+			conn = DriverManager.getConnection(jdbcURL, userName, new String(password));
+			ps = conn.prepareStatement("select sysdate from dual");
+			resultSet = ps.executeQuery();
+			while (resultSet.next()) {
+				resultSet.getString(1);
+			}
+			long sqlExecutionEnd = System.currentTimeMillis();
+			sqlExecutionValues.add(sqlExecutionEnd - sqlExecutionStart);
+		} catch (Exception exception) {
+			System.out.println("Can't connect to Database " + jdbcURL + " (" + userName + ")");
+			System.out.println(exception);
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (Exception exception) {
+					System.out.println("Failed to close resultset");
+					System.out.println(exception);
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (Exception exception) {
+					System.out.println("Failed to close prepared statement");
+					System.out.println(exception);
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception exception) {
+					System.out.println("Failed to close connection");
+					System.out.println(exception);
+				}
+			}
+		}
+	}
+
 	public void run() {
 		long numberOfExecution = executionValues.size();
 		System.out.println("Total Execution              : " + numberOfExecution);
 		Collections.sort(executionValues, new LongComparator());
 		Collections.sort(connectionValues, new LongComparator());
+		Collections.sort(sqlExecutionValues, new LongComparator());
 		System.out.println("Execution time (max/avg/median/min) : " + executionValues.get(executionValues.size() - 1)
 				+ " / " + mean(executionValues) + " / " + median(executionValues) + " / " + executionValues.get(0));
 		System.out.println("Connection time (max/avg/median/min) : " + connectionValues.get(connectionValues.size() - 1)
 				+ " / " + mean(connectionValues) + " / " + median(connectionValues) + " / " + connectionValues.get(0));
+		if (sqlExecutionValues.size() > 0)
+			System.out.println("SQL time (count/max/avg/median/min)    : "
+					+ sqlExecutionValues.size() +" / " + sqlExecutionValues.get(sqlExecutionValues.size() - 1) + " / " + mean(sqlExecutionValues) + " / "
+					+ median(sqlExecutionValues) + " / " + sqlExecutionValues.get(0));
 	}
 
 	public long mean(List<Long> values) {
